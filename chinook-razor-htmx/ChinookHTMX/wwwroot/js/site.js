baf1cdf89bc4c0b1fb5ab6c8e12c9d6632fd6945
Chris Woodruff <cwoodruff@live.com>
2024-04-19 07:16:26 -0400

    updated Artists data table

--- chinook-razor-htmx/ChinookHTMX/wwwroot/js/site.js
+++ chinook-razor-htmx/ChinookHTMX/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    $('#dataTable').DataTable();
+});
